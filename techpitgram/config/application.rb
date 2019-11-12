96c83abffd5a663c2773eadc283aed31f8070499
RieMatsui <funkybass0405@gmail.com>
2019-11-12 04:42:58 +0000

    rails6 and bootstrap4 inculude.

--- techpitgram/config/application.rb
+++ techpitgram/config/application.rb
@@ -9,7 +9,8 @@ Bundler.require(*Rails.groups)
 module Techpitgram
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
+    config.hosts << "8c28282d2a314b68ac04a0dab1d4adfc.vfs.cloud9.ap-southeast-1.amazonaws.com"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
